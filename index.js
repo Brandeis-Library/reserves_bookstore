de93d511512e5e64353c94a8645d37a7ec0ab152
Chris Underwood <cunderwoodmn@gmail.com>
2020-10-23 18:12:09 -0400

    rename Excel sheet gto TestData.xlsx and added ability to have the sheet converted to JSON.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const fs = require('fs');
+//const fs = require('fs');
+XLSX = require('xlsx');
 
-console.log('Can you see me now?');
+(async function () {
+  const workbook = XLSX.readFile('TestData.xlsx');
+  //console.log('workbook ---------- ', workbook);
+  const sheetNames = workbook.SheetNames;
+  const sheetIndex = 1;
+
+  var df = XLSX.utils.sheet_to_json(
+    workbook.Sheets[sheetNames[sheetIndex - 1]]
+  );
+
+  console.log('data ----------------------- ', df);
+  console.log('Can you see me now?');
+})();
