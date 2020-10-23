c5c385fb33d741ba0a8cd7b1f08e3d9549b4b111
Chris Underwood <cunderwoodmn@gmail.com>
2020-10-23 19:53:36 -0400

    add notes about future steps & considerations.

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ XLSX = require('xlsx');
     workbook.Sheets[sheetNames[sheetIndex - 1]]
   );
 
+  // Do a forEach loop over each object in the df array and write to a file.
+
+  // Send a request to Alma (API or SRU) to determine if we Brandeis has an item.
+  // https://developers.exlibrisgroup.com/alma/integrations/sru/
+
+  // Consider getting rid of the IFEE if it is not needed.
+
   console.log('data ----------------------- ', df);
   console.log('Can you see me now?');
 })();
