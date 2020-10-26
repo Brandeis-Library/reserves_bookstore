d760540cc0549582fdbfa81c13327d7fe8d43e5b
Chris Underwood <cunderwoodmn@gmail.com>
2020-10-26 14:27:31 -0400

    add 2 files for storing results, add ability to truncate those files, and write the headers.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const fs = require('fs');
+const fs = require('fs');
 XLSX = require('xlsx');
 
 (async function () {
@@ -10,6 +10,18 @@ XLSX = require('xlsx');
   var df = XLSX.utils.sheet_to_json(
     workbook.Sheets[sheetNames[sheetIndex - 1]]
   );
+
+  // Truncate to_Be_Purchased before appending
+  fs.truncateSync('./to_Be_Purchased.csv');
+
+  // write headers for to_Be_Purchased.csv
+  fs.createWriteStream('./to_Be_Purchased.csv', { flags: 'a' }).write(`ISBN`);
+
+  // Truncate already_Owned before appending
+  fs.truncateSync('./already_Owned.csv');
+
+  // write headers for already_Owned.csv
+  fs.createWriteStream('./already_Owned.csv', { flags: 'a' }).write(`ISBN`);
 
   // Do a forEach loop over each object in the df array and write to a file.
 
