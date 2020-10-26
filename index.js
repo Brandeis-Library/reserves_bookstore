d1881386bce996b5a4f96419951c98c2dbde201b
Chris Underwood <cunderwoodmn@gmail.com>
2020-10-26 17:30:12 -0400

    add code to create output files and populate them with dummy data before truncation if they have not already been created.

--- index.js
+++ index.js
@@ -10,19 +10,37 @@ XLSX = require('xlsx');
   var df = XLSX.utils.sheet_to_json(
     workbook.Sheets[sheetNames[sheetIndex - 1]]
   );
+  try {
+    // Ensure creation of to_Be_Purchased before truncating
+    fs.appendFile('./to_Be_Purchased.csv', 'file created', function (err) {
+      if (err) throw err;
+      console.log('Saved to_be_Purchased!');
+    });
 
-  // Truncate to_Be_Purchased before appending
-  fs.truncateSync('./to_Be_Purchased.csv');
+    // Truncate to_Be_Purchased before appending
+    fs.truncateSync('./to_Be_Purchased.csv');
 
-  // write headers for to_Be_Purchased.csv
-  fs.createWriteStream('./to_Be_Purchased.csv', { flags: 'a' }).write(`ISBN`);
+    // write headers for to_Be_Purchased.csv
+    fs.createWriteStream('./to_Be_Purchased.csv', { flags: 'as' }).write(
+      `ISBN12345`
+    );
 
-  // Truncate already_Owned before appending
-  fs.truncateSync('./already_Owned.csv');
+    // Ensure creation of alreadyOwned before truncating
+    fs.appendFile('./already_Owned.csv', 'file created', function (err) {
+      if (err) throw err;
+      console.log('Saved already_Owned!');
+    });
 
-  // write headers for already_Owned.csv
-  fs.createWriteStream('./already_Owned.csv', { flags: 'a' }).write(`ISBN`);
+    // Truncate already_Owned before appending
+    fs.truncateSync('./already_Owned.csv');
 
+    // write headers for already_Owned.csv
+    fs.createWriteStream('./already_Owned.csv', { flags: 'as' }).write(
+      `ISBN12345`
+    );
+  } catch (error) {
+    console.log('ERROR -------- ', error);
+  }
   // Do a forEach loop over each object in the df array and write to a file.
 
   // Send a request to Alma (API or SRU) to determine if we Brandeis has an item.
